80ffb019f107131291c6de59ea258e7f1256b3d5
franklyn.urey <franklyn.urey@kiumas.net>
2024-02-26 23:21:48 -0600

    S02L24 Data Annotations

--- Xanes.Web/Models/Bank.cs
+++ Xanes.Web/Models/Bank.cs
@@ -1,15 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.ComponentModel.DataAnnotations;
 
 namespace Xanes.Web.Models;
 
 public class Bank
 {
+    [Key]
     public int Id { get; set; }
+    [Required]
     public int CompanyId { get; set; }
+    [Required]
     public string Code { get; set; } = null!;
+    [Required]
     public string Name { get; set; } = null!;
     public decimal BankingCommissionPercentage { get; set; }
     public Guid? BankAccountExcludeUId { get; set; }
+    [Required]
     public bool IsCompany { get; set; }
     public int OrderPriority { get; set; }
     public string LogoBank { get; set; } = null!;
