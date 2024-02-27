e43b7b4939242eb1627621af06a663387f24840f
franklyn.urey <franklyn.urey@kiumas.net>
2024-02-26 23:18:56 -0600

    S02L23A Create Bank Model

---
+++ Xanes.Web/Models/Bank.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace Xanes.Web.Models;
+
+public class Bank
+{
+    public int Id { get; set; }
+    public int CompanyId { get; set; }
+    public string Code { get; set; } = null!;
+    public string Name { get; set; } = null!;
+    public decimal BankingCommissionPercentage { get; set; }
+    public Guid? BankAccountExcludeUId { get; set; }
+    public bool IsCompany { get; set; }
+    public int OrderPriority { get; set; }
+    public string LogoBank { get; set; } = null!;
+}
