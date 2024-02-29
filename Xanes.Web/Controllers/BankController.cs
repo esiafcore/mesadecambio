2e3c61a5d1365f3ba1ae8343b12f39bbde7e71db
franklyn.urey <franklyn.urey@kiumas.net>
2024-02-29 09:31:03 -0600

    S02L33 Get All Banks

--- Xanes.Web/Controllers/BankController.cs
+++ Xanes.Web/Controllers/BankController.cs
@@ -1,12 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Xanes.Web.Data;
+using Xanes.Web.Models;
 
 namespace Xanes.Web.Controllers;
 
 public class BankController : Controller
 {
+    private readonly ApplicationDbContext _db;
+
+    public BankController(ApplicationDbContext db)
+    {
+        _db = db;
+    }
     // GET
     public IActionResult Index()
     {
+        //var dataList = _db.Banks.ToList();
+        List<Bank> dataList = _db.Banks.ToList();
         return View();
     }
 }
