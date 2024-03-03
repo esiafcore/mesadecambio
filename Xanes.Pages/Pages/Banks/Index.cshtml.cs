8990573d40fc379676a3aa23c9885b2ac826e720
franklyn.urey <franklyn.urey@kiumas.net>
2024-03-03 14:51:05 -0600

    S03L057 Create first Razor Page

---
+++ Xanes.Pages/Pages/Banks/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Xanes.Pages.Pages.Banks
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
