c76e97102df91e32c40b6b2875ec8bfdf10ccc26
franklyn.urey <franklyn.urey@kiumas.net>
2024-03-03 15:33:02 -0600

    S03L059 Create Bank UI

---
+++ Xanes.Pages/Pages/Banks/Create.cshtml.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Xanes.Pages.Data;
+using Xanes.Pages.Models;
+
+namespace Xanes.Pages.Pages.Banks
+{
+    public class CreateModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly IConfiguration _configuration;
+        private readonly int _companyId;
+        public Bank Bank { get; set; }
+        public CreateModel(ApplicationDbContext db, IConfiguration configuration)
+        {
+            _db = db;
+            _configuration = configuration;
+            _companyId = _configuration.GetValue<int>("ApplicationSettings:CompanyId");
+        }
+
+        public void OnGet()
+        {
+            //Setear valor por defecto
+            Bank = new Bank()
+            {
+                OrderPriority = 1,
+                BankingCommissionPercentage = 0,
+                CompanyId = _companyId
+            };
+        }
+    }
+}
