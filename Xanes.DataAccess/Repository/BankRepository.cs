d91b9f22c91e067805f2847d079e8bf7bd26d515
franklyn.urey <franklyn.urey@kiumas.net>
2024-03-04 22:01:56 -0600

    S05L073 Implement Bank Repository

---
+++ Xanes.DataAccess/Repository/BankRepository.cs
@@ -0,0 +1,25 @@
+﻿using Xanes.DataAccess.Data;
+using Xanes.DataAccess.Repository.IRepository;
+using Xanes.Models;
+
+namespace Xanes.DataAccess.Repository;
+
+public class BankRepository : Repository<Bank>, IBankRepository
+{
+    private readonly ApplicationDbContext _db;
+
+    public BankRepository(ApplicationDbContext db) : base(db)
+    {
+        _db = db;
+    }
+
+    public void Update(Bank obj)
+    {
+        _db.Banks.Update(obj);
+    }
+
+    public void Save()
+    {
+        _db.SaveChanges();
+    }
+}
