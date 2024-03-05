6d129f69add130eff11895c1adc47d38fd7da8c9
franklyn.urey <franklyn.urey@kiumas.net>
2024-03-04 21:37:41 -0600

    S05L070 IRepository Interface

---
+++ Xanes.DataAccess/Repository/IRepository/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq.Expressions;
+
+namespace Xanes.DataAccess.Repository.IRepository;
+
+internal interface IRepository<T> where T : class
+{
+    IEnumerable<T> GetAll();
+    T Get(Expression<Func<T,bool>> filter);
+    void Add(T entity);
+    //Decisión personal. Tener fuera el método Update
+    //void Update(T entity);
+    void Remove(T entity);
+    void RemoveRange(IEnumerable<T> entities);
+
+
+    
+}
