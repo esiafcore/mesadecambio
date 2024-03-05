1766224c4954f406c067c3e26502b3c3c29e08a9
franklyn.urey <franklyn.urey@kiumas.net>
2024-03-04 21:58:53 -0600

    S05L072 Implement IBankRepository

---
+++ Xanes.DataAccess/Repository/IRepository/IBankRepository.cs
@@ -0,0 +1,9 @@
+﻿using Xanes.Models;
+
+namespace Xanes.DataAccess.Repository.IRepository;
+
+public interface IBankRepository : IRepository<Bank>
+{
+    void Update(Bank obj);
+    void Save();
+}
