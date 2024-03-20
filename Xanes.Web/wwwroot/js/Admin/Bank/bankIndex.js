79792dd554d90b0b7d70f22205fb59a66f7cbf92
franklyn.urey <franklyn.urey@kiumas.net>
2024-03-19 23:12:59 -0600

    S06L102 Load DataTables Part 02 without errors

--- Xanes.Web/wwwroot/js/Admin/Bank/bankIndex.js
+++ Xanes.Web/wwwroot/js/Admin/Bank/bankIndex.js
@@ -3,25 +3,45 @@
 });
 
 function loadDatatable() {
-    let dataTable = new DataTable("#tblData", {
+    let dataTable = $("#tblData");
+
+    dataTable.DataTable({
         "ajax": { url: '/admin/bank/getall' },
         "columns": [
-            { data: 'code', "width": "15%" },
+            { data: 'code', "width": "5%" },
             { data: 'name', "width": "15%" },
-            { data: 'bankingCommissionPercentage', "width": "15%" },
-            { data: 'isCompany', "width": "15%" },
-            { data: 'orderPriority', "width": "15%" }
+            { data: 'bankingCommissionPercentage', "width": "5%" },
+            { data: 'isCompany', "width": "5%" },
+            { data: 'orderPriority', "width": "5%" }
+        ],
+        "info": false,
+        "ordering": true,
+        "paging": true,
+        "order": [[1, 'asc'], [0, 'desc']],
+        "columnDefs": [
+            {
+                target: 0,
+                visible: true,
+                searchable: true,
+                orderable: true
+            },
+            {
+                target: 1,
+                orderable: false
+            },
+            {
+                target: 2,
+                orderable: false
+            },
+            {
+                target: 3,
+                orderable: false
+            },
+            {
+                target: 4,
+                orderable: false
+            },
+
         ]
     });
-
-    //dataTable = $("#tblData").DataTable({
-    //    "ajax": { url: '/admin/bank/getall' },
-    //    "columns": [
-    //        { data: 'code', "width": "15%" },
-    //        { data: 'name', "width": "15%" },
-    //        { data: 'bankingCommissionPercentage', "width": "15%" },
-    //        { data: 'isCompany', "width": "15%" },
-    //        { data: 'orderPriority', "width": "15%" }
-    //    ]
-    //});
 }
