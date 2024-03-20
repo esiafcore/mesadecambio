d8cd4c6f8f26afef9445e95e5870962f0e9d6ae3
franklyn.urey <franklyn.urey@kiumas.net>
2024-03-19 20:22:37 -0600

    S06L099B Model, CRUD and Views of Customers Sectors Part 3

--- Xanes.Models/Currency.cs
+++ Xanes.Models/Currency.cs
@@ -9,37 +9,40 @@ namespace Xanes.Models;
 [Table("currencies", Schema = "cnf")]
 public class Currency : EntityInactivated, ICloneable
 {
-    [MaxLength(15, ErrorMessage = "Longitud máxima del campo {0} es {1}")]
     [Required(ErrorMessage = "El campo {0} es requerido")]
     [DisplayName(displayName: "Código Iso")]
+    [MaxLength(15, ErrorMessage = "Longitud máxima del campo {0} es {1}")]
     public string CodeIso { get; set; } = null!;
 
-    [MaxLength(15, ErrorMessage = "Longitud máxima del campo {0} es {1}")]
     [Required(ErrorMessage = "El campo {0} es requerido")]
     [DisplayName(displayName: "Código")]
+    [MaxLength(15, ErrorMessage = "Longitud máxima del campo {0} es {1}")]
     public string Code { get; set; } = null!;
 
-    [MaxLength(5, ErrorMessage = "Longitud máxima del campo {0} es {1}")]
     [Required(ErrorMessage = "El campo {0} es requerido")]
     [DisplayName(displayName: "Símbolo")]
+    [MaxLength(5, ErrorMessage = "Longitud máxima del campo {0} es {1}")]
     public string Abbreviation { get; set; } = null!;
 
-    [MaxLength(150, ErrorMessage = "Longitud máxima del campo {0} es {1}")]
     [Required(ErrorMessage = "El campo {0} es requerido")]
     [DisplayName(displayName: "Nombre")]
+    [MaxLength(150, ErrorMessage = "Longitud máxima del campo {0} es {1}")]
     public string Name { get; set; } = null!;
-    [MaxLength(150, ErrorMessage = "Longitud máxima del campo {0} es {1}")]
+
     [DisplayName(displayName: "Nombre en Singular")]
+    [MaxLength(150, ErrorMessage = "Longitud máxima del campo {0} es {1}")]
     public string NameSingular { get; set; } = null!;
-    [MaxLength(150, ErrorMessage = "Longitud máxima del campo {0} es {1}")]
+
     [DisplayName(displayName: "Nombre Foráneo")]
-    public string NameForeign { get; set; } = null!;
     [MaxLength(150, ErrorMessage = "Longitud máxima del campo {0} es {1}")]
+    public string NameForeign { get; set; } = null!;
+
     [DisplayName(displayName: "Nombre Foráneo en Singular")]
+    [MaxLength(150, ErrorMessage = "Longitud máxima del campo {0} es {1}")]
     public string NameForeignSingular { get; set; } = null!;
 
-    [Required(ErrorMessage = "El campo {0} es requerido")]
     [DisplayName(displayName: "Número")]
+    [Required(ErrorMessage = "El campo {0} es requerido")]
     public int Numeral { get; set; }
 
     public virtual object Clone()
