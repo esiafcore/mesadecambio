99c8127fd39e1e9075b97aa5455ed242844afa8a
franklyn.urey <franklyn.urey@kiumas.net>
2024-03-26 16:51:52 -0600

    S06L104 Edit Bank Link in DataTable

--- Xanes.Web/wwwroot/js/Admin/Bank/bankIndex.js
+++ Xanes.Web/wwwroot/js/Admin/Bank/bankIndex.js
@@ -1,18 +1,37 @@
 ﻿document.addEventListener("DOMContentLoaded", function () {
     loadDatatable();
+
+    //Habilitar Tooltip
+    fnEnableTooltip();
 });
 
 function loadDatatable() {
-    let dataTable = $("#tblData");
-
-    dataTable.DataTable({
+    let dataTable = new DataTable("#tblData",{
         "ajax": { url: '/admin/bank/getall' },
+        "dataSrc": 'data',
         "columns": [
             { data: 'code', "width": "5%" },
             { data: 'name', "width": "15%" },
             { data: 'bankingCommissionPercentage', "width": "5%" },
             { data: 'isCompany', "width": "5%" },
-            { data: 'orderPriority', "width": "5%" }
+            { data: 'orderPriority', "width": "5%" },
+            {
+                data: 'id',
+                "render": function (data) {
+                    return `<div class="btn-group" role="group">
+                        <a href="/admin/bank/upsert?id=${data}" class="btn btn-primary py-1 px-3 my-0 mx-2"
+                           data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="Editar">
+                            <i class="bi bi-pencil-square fs-4"></i>
+                        </a>
+                        <a href="/admin/bank/delete?id=${data}" class="btn btn-danger py-1 px-3 my-0 mx-2" 
+                           data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="Eliminar">
+                            <i class="bi bi-trash-fill fs-4"></i>
+                        </a>
+
+                    </div>`
+                },
+                "width": "15%" 
+            }
         ],
         "info": false,
         "ordering": true,
