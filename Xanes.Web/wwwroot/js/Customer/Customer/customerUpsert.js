e8792c52833092b82bdff0cd880262183df46c38
franklyn.urey <franklyn.urey@kiumas.net>
2024-04-25 08:25:23 -0600

    S06L106 Fix Input Value NAN with valueAsNumber Part01

--- Xanes.Web/wwwroot/js/Customer/Customer/customerUpsert.js
+++ Xanes.Web/wwwroot/js/Customer/Customer/customerUpsert.js
@@ -1,4 +1,5 @@
-﻿document.addEventListener("DOMContentLoaded", () => {
+﻿
+document.addEventListener("DOMContentLoaded", () => {
 
     let inputId = document.getElementById("DataModel_Id");
     $("#DataModel_CategoryId").select2(select2Options);
@@ -12,21 +13,53 @@
     //Setear form-floating para Select2
     select2Floating();
 
+    //Setear Event Handler a utilizar
+    inputFirstName.addEventListener("change", naturalNames_onChange);
+    inputSecondName.addEventListener("change", naturalNames_onChange);
+    inputLastName.addEventListener("change", naturalNames_onChange);
+    inputSecondSurname.addEventListener("change", naturalNames_onChange);
+
+    personType_onClick(personLegalRad);
+
     //setear el foco al crear o editar
     document.getElementById("DataModel_Code").focus();
-    personType_onClick(personLegalRad);
 });
 
 function personType_onClick(objElem) {
-    let currentValue = Number(objElem.value);
+    let currentValue = objElem.valueAsNumber;
 
     if (currentValue == PersonType.Natural) {
-        personNaturalDiv.style.display = "" //styleShow;
+        personNaturalDiv.style.display = styleShow;
         personLegalDiv.style.display = styleHide;
     }
     else if (currentValue == PersonType.Legal) {
-        personLegalDiv.style.display = "" //styleShow;
+        personLegalDiv.style.display = styleShow;
         personNaturalDiv.style.display = styleHide;
     }
 }
 
+
+function naturalNames_onChange(e) {
+    //Hay dato y tipo de persona es natural
+    if (e.target.value && personNaturalRad.checked) {
+        //Concatenar los 4 campos
+        let personName = "";
+        if (inputFirstName.value) {
+            personName = inputFirstName.value.trim();
+        }
+
+        if (inputSecondName.value) {
+            personName = `${personName.trim()} ${inputSecondName.value.trim() }`;
+        }
+
+        if (inputLastName.value) {
+            personName = `${personName.trim()} ${inputLastName.value.trim()}`;
+        }
+
+        if (inputSecondSurname.value) {
+            personName = `${personName.trim()} ${inputSecondSurname.value.trim()}`;
+        }
+
+        console.log(personName);
+    }
+}
