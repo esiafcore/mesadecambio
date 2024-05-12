f44d53603674d95f884696b74087f1f50fa30dd9
AUrey <angel123urey@gmail.com>
2024-05-12 12:59:39 -0600

    S06L106 FIX - BusinessExecutive View Create

---
+++ Xanes.DataAccess/Migrations/PgSql/20240512174740_CheckPoint_P001.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Xanes.DataAccess.Migrations.PgSql
+{
+    /// <inheritdoc />
+    public partial class CheckPoint_P001 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                schema: "cnf",
+                table: "companies",
+                keyColumn: "id",
+                keyValue: 1,
+                column: "createddate",
+                value: new DateTime(2024, 5, 12, 17, 47, 35, 144, DateTimeKind.Utc).AddTicks(4882));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                schema: "cnf",
+                table: "companies",
+                keyColumn: "id",
+                keyValue: 1,
+                column: "createddate",
+                value: new DateTime(2024, 5, 10, 20, 58, 56, 878, DateTimeKind.Utc).AddTicks(3197));
+        }
+    }
+}
