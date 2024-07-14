3f493cc9edc1ac601f0d823ac520d23331533eba
RPoveda <rafael.poveda@kiumas.net>
2024-07-14 17:31:33 -0600

    FIX - SCROLL X y Y indexReport js

--- Xanes.Web/wwwroot/js/Exchange/SystemInformation/indexReport.js
+++ Xanes.Web/wwwroot/js/Exchange/SystemInformation/indexReport.js
@@ -3,5 +3,10 @@ let containerMain;
 
 document.addEventListener("DOMContentLoaded", async () => {
     containerMain = document.querySelector("#containerMain");
-    containerMain.classList = "container-fluid p-0";  
+    containerMain.classList = "container-fluid p-0";
+
+    // buscar la etiqueta MAIN y quitar las clases
+    const main = containerMain.querySelector("main");
+    main.classList = "";
+    main.classList = "p-0";
 });
